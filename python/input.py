d1eace77328cad8a457f8039539a3505ba9dba9f
jv1175480-sys <jv1175480@gmail.com>
2026-07-07 16:10:19 -0400

    Add files via upload

---
+++ python/input.py
@@ -0,0 +1,5 @@
+name = input("please enter your name")
+print("hello my name is " + name)
+
+x= float(input("please input a number:"))
+print((5 * 5) * x)
---
+++ python/new.py
@@ -0,0 +1,3 @@
+print(input(1)= True
+input(2)= False
+input(3)= False)
